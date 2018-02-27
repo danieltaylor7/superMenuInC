94d45de8940ad697b21778322cf08fbd15f83d9c
Daniel Taylor <danieltaylor7@mail.weber.edu>
2018-02-27 12:37:06 -0700

    Modify print statement

---
+++ superMenu.c
@@ -0,0 +1,38 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  superMenu.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/27/2018 11:48:33 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Daniel Taylor (), danieltaylor7@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+void HelloSuper();
+
+// Main Function
+int main()
+{
+    HelloSuper();
+    return 0;
+}
+// Function Definitions
+void HelloSuper()
+{
+    printf("Hello super program in C\n");
+    return;
+}
+
+
